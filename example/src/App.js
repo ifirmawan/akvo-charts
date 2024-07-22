721142d1a54d200655fc5a6e8c2294c2bcb6cc8e
dedenbangkit <mail@dedenbangkit.com>
2024-07-22 15:55:20 +0700

    Fix deprecated package

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'akvo-charts'
 import 'akvo-charts/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text='Create React Library' />
 }
 
 export default App
