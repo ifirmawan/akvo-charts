ab89e7437e8220ea391106e139594b61d4d45d1e
ifirmawan <hellofirmawan@gmail.com>
2024-07-25 13:19:03 +0700

    test: add basePath

--- example/next.config.mjs
+++ example/next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
   output: 'export',
-  basePath: 'akvo-charts',
+  basePath: '/akvo-charts',
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
