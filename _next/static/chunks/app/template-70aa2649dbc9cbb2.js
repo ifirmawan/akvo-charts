4a32e54a21a02ae5ce1ff38f68b293d9f4c4151b
ifirmawan <hellofirmawan@gmail.com>
2024-07-26 18:55:22 +0700

    Updates

---
+++ _next/static/chunks/app/template-70aa2649dbc9cbb2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[489],{5865:function(e,n,s){Promise.resolve().then(s.bind(s,7920)),Promise.resolve().then(s.bind(s,1254)),Promise.resolve().then(s.bind(s,6672)),Promise.resolve().then(s.bind(s,4856)),Promise.resolve().then(s.t.bind(s,8957,23))}},function(e){e.O(0,[130,408,74,971,23,744],function(){return e(e.s=5865)}),_N_E=e.O()}]);
